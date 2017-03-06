b3845670d857ae02d431450b02be45041206c3dd
nirajp1 <nirajp1@uci.edu>
2017-03-06 12:45:42 -0800

    Modified GcodeGenerator.py to convert pixel coordinates to actual measurements

--- Software/GcodeGenerator.py
+++ Software/GcodeGenerator.py
@@ -5,6 +5,8 @@ class GcodeGenerator:
     def __init__(self, clusters):
         self._clusters = clusters
         self._feed_rate = 20000
+        self._CELL_WIDTH_MM = 10
+        self._WALL_WIDTH_MM = 10
 
     def generate_simple_cmds(self, filename):
         with open(filename, 'w') as f:
@@ -12,7 +14,10 @@ class GcodeGenerator:
                 sorted_v = sorted(v, key = itemgetter(0,1))
                 
                 for x,y in sorted_v:
+                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + ' y' + str(y* self._CELL_WIDTH_MM) + ' f' + str(self._feed_rate) + '\n')
                     f.write('m17\n')
-                    f.write('g1 x' + str(x) + ' y' + str(y) + ' f' + str(self._feed_rate) + '\n')
                     f.write('m16\n')
+
+
+    
                             
