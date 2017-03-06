97073560ba7698d7e6b2d0073097c34e228b9d55
nirajp1 <nirajp1@uci.edu>
2017-03-06 13:55:52 -0800

    Modifed GcodeGenerator to handle m17 and m16 moving too fast

--- Software/GcodeGenerator.py
+++ Software/GcodeGenerator.py
@@ -4,7 +4,7 @@ from operator import itemgetter
 class GcodeGenerator:
     def __init__(self, clusters):
         self._clusters = clusters
-        self._feed_rate = 20000
+        self._FEED_RATE = 20000
         self._CELL_WIDTH_MM = 10
         self._WALL_WIDTH_MM = 10
 
@@ -14,8 +14,16 @@ class GcodeGenerator:
                 sorted_v = sorted(v, key = itemgetter(0,1))
                 
                 for x,y in sorted_v:
-                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + ' y' + str(y* self._CELL_WIDTH_MM) + ' f' + str(self._feed_rate) + '\n')
+                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                            ' f' + str(self._FEED_RATE) + '\n')
                     f.write('m17\n')
+                    f.write('g1 x' + str((x * self._CELL_WIDTH_MM) + .5) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                            ' f1\n')
+                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                            ' f1\n')
                     f.write('m16\n')
 
 
