cf398bea79646264738718aa81d4ab8a93a17f45
nirajp1 <nirajp1@uci.edu>
2017-03-13 15:50:00 -0700

    Added offset to GcodeGenerator.py for smaller display

--- Software/GcodeGenerator.py
+++ Software/GcodeGenerator.py
@@ -7,6 +7,8 @@ class GcodeGenerator:
         self._FEED_RATE = 20000
         self._CELL_WIDTH_MM = 10
         self._WALL_WIDTH_MM = 10
+        self._X_OFFSET = 120
+        self._Y_OFFSET = 80
 
     def generate_simple_cmds(self, filename):
         with open(filename, 'w') as f:
@@ -14,15 +16,15 @@ class GcodeGenerator:
                 sorted_v = sorted(v, key = itemgetter(0,1))
                 
                 for x,y in sorted_v:
-                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + \
-                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM + self._X_OFFSET) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM + self._Y_OFFSET) + \
                             ' f' + str(self._FEED_RATE) + '\n')
                     f.write('m17\n')
-                    f.write('g1 x' + str((x * self._CELL_WIDTH_MM) + .5) + \
-                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                    f.write('g1 x' + str((x * self._CELL_WIDTH_MM) + self._X_OFFSET + .5) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM + self._Y_OFFSET) + \
                             ' f1\n')
-                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM) + \
-                            ' y' + str(y * self._CELL_WIDTH_MM) + \
+                    f.write('g1 x' + str(x * self._CELL_WIDTH_MM + self._X_OFFSET) + \
+                            ' y' + str(y * self._CELL_WIDTH_MM + self._Y_OFFSET) + \
                             ' f1\n')
                     f.write('m16\n')
 
